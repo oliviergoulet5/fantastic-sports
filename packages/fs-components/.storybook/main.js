4d37aa6db361fffc8c9b7ef5f168318119ec91e0
Olivier Goulet <olivier_goulet@icloud.com>
2022-08-01 15:05:08 -0400

    Add test component to storybook

--- packages/fs-components/.storybook/main.js
+++ packages/fs-components/.storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/stories/**/*.stories.mdx",
+    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
@@ -11,5 +11,14 @@ module.exports = {
   "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-webpack5"
+  },
+  "typescript": {
+    "check": false,
+    "checkOptions": {},
+    "reactDocgen": "react-docgen-typescript",
+    "reactDocgenTypescriptOptions": {
+      "shouldExtractLiteralValuesFromEnum": true,
+      "propFilter": (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true)
+    }
   }
 }
---
+++ packages/fs-components/src/components/NavigationBar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface Props {
+
+}
+
+function NavigationBar({}: Props) {
+    return (
+        <nav>
+            Hello, world!
+        </nav>
+    )
+}
+
+export default NavigationBar;
