94a897bc16ae5d40959c3252c0455d0497c9d13a
Olivier Goulet <olivier_goulet@icloud.com>
2022-08-01 15:34:14 -0400

    Create a component library (#9)
    * Add fs-components package
    
    * Add storybook to fs-components
    
    * Add test component to storybook
    
    * Add SASS to fs-components

---
+++ packages/fs-components/src/components/NavigationBar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../sass/base/_typography.scss";
+
+interface Props {
+
+}
+
+function NavigationBar({}: Props) {
+    return (
+        <nav>
+            Hello, world!
+        </nav>
+    )
+}
+
+export default NavigationBar;
---
+++ packages/fs-components/src/stories/components/navigation-bar.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import NavigationBar from "../../components/NavigationBar";
+
+export default {
+    title: "Components/NavigationBar",
+    component: NavigationBar
+}
+
+export const NavigationBarComponent = () => <NavigationBar />
